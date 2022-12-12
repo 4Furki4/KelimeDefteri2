f8c99973b022c54a418e0ca1363b7cbcc8c1dd1b
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-12-13 00:22:24 +0300

    First commit, just home and notfound components are added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
