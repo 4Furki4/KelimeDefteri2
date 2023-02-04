68e809b0df8471f9cfd0ec6c22011d8ddfa4f565
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-02-05 02:45:55 +0300

    redundant folders, files and imports were removed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppNavComponent } from './app-nav/app-nav.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatInputModule } from '@angular/material/input';
-import { MatTabsModule } from '@angular/material/tabs';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { HomeModule } from './home/home.module';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { RequestInterceptor } from './HttpHandlers/request.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { CreateRecordModule } from './create-record/create-record.module';
+
 import { UIModule } from './ui/ui.module';
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
-    AppNavComponent,
     ToolbarComponent
   ],
   imports: [
@@ -32,16 +21,7 @@ import { UIModule } from './ui/ui.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     NgxSpinnerModule,
-    MatIconModule,
-    MatSlideToggleModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatInputModule,
-    MatTabsModule,
     HttpClientModule,
-    MatToolbarModule,
-    HomeModule,
     UIModule
   ],
   providers: [
